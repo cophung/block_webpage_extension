7b4ef0a55f6e368c472f45e41e8b726e76f50d01
cophung <phungtheco1@gmail.com>
2020-10-22 11:09:47 +0700

    notification-complete

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
+let manifest = browser.runtime.getManifest();
 let listUrl = ["https://www.youtube.com/*", "https://thanhnien.vn/*"];
 
 function blockSite(requestDetails) {
-  console.log("Loading: " + requestDetails.url);
+  browser.notifications.create("", {
+    title: `${manifest.name}`,
+    iconUrl: browser.runtime.getURL("icons/icon-60.png"),
+    message: `Trang ${requestDetails.url.slice(0, -1)} của bạn đã bị chặn`,
+    type: "basic",
+  });
   return { cancel: true };
 }
 
