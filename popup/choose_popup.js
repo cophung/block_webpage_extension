07ab01091b74f1fd1816fd7f50fa0aba342f7f8a
cophung <phungtheco1@gmail.com>
2020-10-23 17:16:19 +0700

    complete delete url

--- popup/choose_popup.js
+++ popup/choose_popup.js
@@ -1,4 +1,7 @@
+let manifest = browser.runtime.getManifest();
+
 document.getElementById("btnAdd").disabled = true;
+document.getElementById("btn-delete").disabled = true;
 
 //begin input new url
 document.getElementById("btnAdd").addEventListener("click", function () {
@@ -14,14 +17,36 @@ document.getElementById("btnAdd").addEventListener("click", function () {
   }
 
   if (urls) {
-    urls.push(newUrl);
+    if (urls.find((item) => item === newUrl)) {
+      browser.notifications.create("", {
+        title: `${manifest.name}`,
+        iconUrl: browser.runtime.getURL("icons/icon-60.png"),
+        message: `Trang ${newUrl} của bạn đã tồn tại trong danh sách chặn`,
+        type: "basic",
+      });
+    } else {
+      urls.push(newUrl);
+      browser.notifications.create("", {
+        title: `${manifest.name}`,
+        iconUrl: browser.runtime.getURL("icons/icon-60.png"),
+        message: `Trang ${newUrl} của bạn đã được thêm vào danh sách chặn`,
+        type: "basic",
+      });
+    }
   } else {
     localStorage.setItem("urls", JSON.stringify([]));
     urls = JSON.parse(localStorage.getItem("urls"));
     urls.push(newUrl);
+    browser.notifications.create("", {
+      title: `${manifest.name}`,
+      iconUrl: browser.runtime.getURL("icons/icon-60.png"),
+      message: `Trang ${newUrl} của bạn đã được thêm vào danh sách chặn`,
+      type: "basic",
+    });
   }
 
   localStorage.setItem("urls", JSON.stringify(urls));
+
   browser.runtime.reload();
 });
 //end input new url
@@ -50,10 +75,66 @@ document
     }
 
     for (let i = 0; i < urls.length; i++) {
-      listUrl += `<tr><td>${urls[i]}</td><td><button>Delete</button></td></tr>`;
+      listUrl += `<tr><td>${urls[i]}</td></tr>`;
     }
 
     document.getElementById("table-url").innerHTML = listUrl;
     //end show table
   });
 //end show all
+
+//begin button delete
+document.getElementById("url-delete").addEventListener("change", function () {
+  if (document.getElementById("url-delete").value === "") {
+    document.getElementById("btn-delete").disabled = true;
+  } else {
+    document.getElementById("btn-delete").disabled = false;
+  }
+});
+//end button delete
+
+//begin input delete url
+document.getElementById("btn-delete").addEventListener("click", function () {
+  let urlDelete = document.getElementById("url-delete").value;
+  let urls = JSON.parse(localStorage.getItem("urls"));
+
+  let patternHttps = /https/gim;
+  urlDelete = urlDelete.replace(/\s/g, "");
+  let resultHttps = patternHttps.test(urlDelete);
+
+  if (!resultHttps) {
+    urlDelete = "https://" + urlDelete + "/";
+  }
+
+  if (urls) {
+    let result = urls.find((item) => item === urlDelete);
+    if (result !== undefined) {
+      let newArrayUrls = urls.filter((item) => item !== urlDelete);
+      localStorage.setItem("urls", JSON.stringify(newArrayUrls));
+
+      browser.notifications.create("", {
+        title: `${manifest.name}`,
+        iconUrl: browser.runtime.getURL("icons/icon-60.png"),
+        message: `Trang ${urlDelete} đã được xóa khỏi danh sách chặn`,
+        type: "basic",
+      });
+    } else {
+      browser.notifications.create("", {
+        title: `${manifest.name}`,
+        iconUrl: browser.runtime.getURL("icons/icon-60.png"),
+        message: `Trang ${urlDelete} của bạn không có trong danh sách chặn`,
+        type: "basic",
+      });
+    }
+  } else {
+    browser.notifications.create("", {
+      title: `${manifest.name}`,
+      iconUrl: browser.runtime.getURL("icons/icon-60.png"),
+      message: `Trang ${urlDelete} của bạn không có trong danh sách chặn`,
+      type: "basic",
+    });
+  }
+
+  browser.runtime.reload();
+});
+//end input delete url
